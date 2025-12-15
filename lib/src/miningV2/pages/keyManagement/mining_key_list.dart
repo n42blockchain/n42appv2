b3a1b336b7cb215c283648ad95c54b5172153279
jiangyiwei <342707482@qq.com>
2025-12-15 17:34:15 +0800

    jyw edit

--- lib/src/miningV2/pages/keyManagement/mining_key_list.dart
+++ lib/src/miningV2/pages/keyManagement/mining_key_list.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:n42appv2/src/miningV2/pages/keyManagement/mining_output_tip.dart';
 import 'package:n42appv2/src/miningV2/provider/mining_v2_provider.dart';
 import 'package:n42appv2/src/utils/sp_util.dart';
 import 'package:n42appv2/src/utils/theme_adapter.dart';
+import 'package:n42appv2/src/utils/toast_utils.dart';
 import 'package:n42appv2/src/widgets/app_bar_widget.dart';
 import 'package:n42appv2/src/widgets/container_widget.dart';
 import 'package:n42appv2/generated/l10n.dart';
@@ -58,55 +60,91 @@ class _MiningKeyListState extends State<MiningKeyList> {
           return ContainerStyle1(
             context,
             padding: EdgeInsets.all(ScreenUtil().setWidth(30)),
-            child:Row(
+            margin: EdgeInsets.symmetric(vertical: ScreenUtil().setWidth(16)),
+            child:Column(
               children: [
-                Expanded(
-                  flex: 1,
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    crossAxisAlignment: CrossAxisAlignment.start,
+                Row(
+                  children: [
+                    Expanded(
+                      flex: 1,
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(miningKeyList[index],
+                            style: TextStyle(
+                              color: AppThemeUtils.getColorByKey(context, AppThemeKeys.itemTextColor.name),
+                              fontSize: ScreenUtil().setSp(32),
+                            ),
+                          ),
+                          Text(
+                            isMining?
+                            S.of(context).g_key_193:
+                            S.of(context).g_mining_key_102,
+                            style: TextStyle(
+                              color: AppThemeUtils.getColorByKey(context, isMining?AppThemeKeys.rightTextColor.name:AppThemeKeys.errorTextColor.name),
+                              fontSize: ScreenUtil().setSp(30),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    SizedBox(width: 10,),
+                    if(isMining==false)
+                      InkWell(
+                        onTap: (){
+                          removeKey(index);
+                        },
+                        child: Icon(
+                          Icons.delete_forever_outlined,
+                          color: AppThemeUtils.getColorByKey(context, AppThemeKeys.errorTextColor.name),
+                          size: ScreenUtil().setWidth(40),
+                        ),
+                      ),
+                    if(isMining)
+                      InkWell(
+                        onTap: (){
+                          Navigator.push(context, MaterialPageRoute(builder: (context)=>MiningOutputTip(value: keyValue['keypart'],)));
+                        },
+                        child: Icon(
+                          Icons.output_outlined,
+                          color: AppThemeUtils.getColorByKey(context, AppThemeKeys.mainBlueColor.name),
+                          size: ScreenUtil().setWidth(40),
+                        ),
+                      ),
+                  ],
+                ),
+                Divider(
+                  height: ScreenUtil().setWidth(20),
+                  color: AppThemeUtils.getColorByKey(context, AppThemeKeys.dividerColor.name),
+                  indent: 0,
+                  endIndent: 0,
+                ),
+                Container(
+                  child: Row(
                     children: [
-                      Text(miningKeyList[index],
+                      Expanded(
+                        child: Text('PublicKey:${keyValue['keypart']['publicKey']}',
                         style: TextStyle(
                           color: AppThemeUtils.getColorByKey(context, AppThemeKeys.itemTextColor.name),
                           fontSize: ScreenUtil().setSp(32),
                         ),
-                      ),
-                      Text(
-                        isMining?
-                        S.of(context).g_key_193:
-                        S.of(context).g_mining_key_102,
-                        style: TextStyle(
-                          color: AppThemeUtils.getColorByKey(context, isMining?AppThemeKeys.rightTextColor.name:AppThemeKeys.errorTextColor.name),
-                          fontSize: ScreenUtil().setSp(30),
+                      ),),
+                      SizedBox(width: 10,),
+                      InkWell(
+                        onTap: (){
+                          Clipboard.setData(ClipboardData(text: keyValue['keypart']['publicKey']));
+                          ToastUtils.show(S.of(context).copy);
+                        },
+                        child: Icon(
+                          Icons.copy,
+                          color: AppThemeUtils.getColorByKey(context, AppThemeKeys.mainBlueColor.name),
+                          size: ScreenUtil().setWidth(40),
                         ),
                       ),
                     ],
                   ),
                 ),
-                SizedBox(width: 10,),
-                if(isMining==false)
-                InkWell(
-                  onTap: (){
-                    removeKey(index);
-                  },
-                  child: Icon(
-                    Icons.delete_forever_outlined,
-                    color: AppThemeUtils.getColorByKey(context, AppThemeKeys.errorTextColor.name),
-                    size: ScreenUtil().setWidth(40),
-                  ),
-                ),
-                if(isMining)
-                  InkWell(
-                    onTap: (){
-                      Navigator.push(context, MaterialPageRoute(builder: (context)=>MiningOutputTip(value: keyValue['keypart'],)));
-                    },
-                    child: Icon(
-                      Icons.output_outlined,
-                      color: AppThemeUtils.getColorByKey(context, AppThemeKeys.mainBlueColor.name),
-                      size: ScreenUtil().setWidth(40),
-                    ),
-                  ),
               ],
             ),
           );
