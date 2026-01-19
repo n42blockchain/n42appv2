d516013a96374edbaf71ee61f88b6975d5fa9028
jiangyiwei <342707482@qq.com>
2026-01-19 10:15:31 +0800

    jyw edit

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -104,7 +104,6 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.9.0")
     implementation("com.squareup.okhttp3:okhttp:4.11.0")
-    //implementation(files("libs/mobile-sdk-release.aar"))
     implementation(files("libs/mobile-sdk-android.aar"))
     implementation(files("libs/evm.aar"))
 }
