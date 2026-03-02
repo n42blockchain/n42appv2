87766d6e5aac4136b7afb5cfc5893177ff865a30
jiangyiwei <342707482@qq.com>
2026-03-02 17:13:03 +0800

    fix: repair navigation, Dio initialization, and mining crash

--- lib/features/mining_v2/pages/key_management/mining_import.dart
+++ lib/features/mining_v2/pages/key_management/mining_import.dart
@@ -40,7 +40,7 @@ class _MiningImportState extends ConsumerState<MiningImport> {
   void initState() {
     // TODO: implement initState
     super.initState();
-    _encryptedDataController.text='{"version":"1","timestamp":"2026-01-06T06:17:27.242127Z","kdf":{"name":"pbkdf2","params":{"iterations":150000,"dklen":32},"salt":"tsYiFXiXMvEYZooGWPgttA=="},"cipher":{"name":"aes-256-gcm","iv":"bS/oYySmQN06y8V6"},"ciphertext":"QTdTjLDB50YcTW+rBObQoKDBlvTYBIv6lNR4TgdHdSI4Mq4yvsrcgBgyIDCsTkMMthpz3QHO21yplLBtZQcz6K60arMrTWV0AkREiDxw3bH6/dsa5l+qTV7ridijom8dwSUGKSMYLEYzRAwSdx2L7HPoG8ImvQZZiwhm+sTzmLQ/TH47zpS7UzeMVrKLCmh2tTxuPzR0DO7LmQvLRz8JlaX29mvLmeIEKpLTTne9pC8QAySjc7LutjsBPSEekxRYUFTGNhykkn4ahAMZjbiFGVJTyvnly4VDPDA4BfILW+444HmQrVuy+BJX6z4g69r62HsFGic5TmwhVo66/Eh6PFLqR+OXsae7WfKezozrmCJsWrsVeyozu53kRK9rWFUMaZgDKX60ymzx4TyM1O4zCDCv1sN/luS3xinOtkm3wqMR79C4Xx5tMo3I46ODrUArrb8RRAOBHZbEewX3RynDfnqgnMY++q7NA0is","tag":"4UX62Xy5HPal1AtekQFQIQ=="}';
+    //_encryptedDataController.text='{"version":"1","timestamp":"2026-01-06T06:17:27.242127Z","kdf":{"name":"pbkdf2","params":{"iterations":150000,"dklen":32},"salt":"tsYiFXiXMvEYZooGWPgttA=="},"cipher":{"name":"aes-256-gcm","iv":"bS/oYySmQN06y8V6"},"ciphertext":"QTdTjLDB50YcTW+rBObQoKDBlvTYBIv6lNR4TgdHdSI4Mq4yvsrcgBgyIDCsTkMMthpz3QHO21yplLBtZQcz6K60arMrTWV0AkREiDxw3bH6/dsa5l+qTV7ridijom8dwSUGKSMYLEYzRAwSdx2L7HPoG8ImvQZZiwhm+sTzmLQ/TH47zpS7UzeMVrKLCmh2tTxuPzR0DO7LmQvLRz8JlaX29mvLmeIEKpLTTne9pC8QAySjc7LutjsBPSEekxRYUFTGNhykkn4ahAMZjbiFGVJTyvnly4VDPDA4BfILW+444HmQrVuy+BJX6z4g69r62HsFGic5TmwhVo66/Eh6PFLqR+OXsae7WfKezozrmCJsWrsVeyozu53kRK9rWFUMaZgDKX60ymzx4TyM1O4zCDCv1sN/luS3xinOtkm3wqMR79C4Xx5tMo3I46ODrUArrb8RRAOBHZbEewX3RynDfnqgnMY++q7NA0is","tag":"4UX62Xy5HPal1AtekQFQIQ=="}';
   }
   @override
   void dispose() {
